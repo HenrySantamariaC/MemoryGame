efcd22cc788e5a0bfd2ac3495ddf1a232be59674
unknown <hsantamariaca@unprg.edu.pe>
2022-05-31 03:35:19 -0500

    Despliegue

---
+++ precache-manifest.93b68f2e92f5ed342be6b28efd5cfe9e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae8670761a015962be2",
+    "url": "/MemoryGame/css/app.1d7ebe37.css"
+  },
+  {
+    "revision": "79781023bb2f271a1944",
+    "url": "/MemoryGame/css/chunk-vendors.5d3035bc.css"
+  },
+  {
+    "revision": "5e4d7e4aaa10594aeec876d6b398c41e",
+    "url": "/MemoryGame/fonts/CYNGamer.5e4d7e4a.ttf"
+  },
+  {
+    "revision": "5f9baf7d026ecc779ba90da278bbc2f9",
+    "url": "/MemoryGame/img/MemoryGameFavicon.5f9baf7d.png"
+  },
+  {
+    "revision": "d79bc9ffa7451578621b505bd544486c",
+    "url": "/MemoryGame/img/MemoryGameLogo.d79bc9ff.png"
+  },
+  {
+    "revision": "1234438040e1097ae39af9332df3060d",
+    "url": "/MemoryGame/img/PokeApi.12344380.png"
+  },
+  {
+    "revision": "c0c05ba4340b821c01c6152a3802e15a",
+    "url": "/MemoryGame/img/RickAndMortyApi.c0c05ba4.png"
+  },
+  {
+    "revision": "3bf0e76ef7ad70eeec5a680a9325fe6f",
+    "url": "/MemoryGame/img/back.3bf0e76e.svg"
+  },
+  {
+    "revision": "e71e0ea6164c3057f1e952eed71894e1",
+    "url": "/MemoryGame/img/emojiCry.e71e0ea6.svg"
+  },
+  {
+    "revision": "4e573337971fe94e7eea57e04c3a4ca7",
+    "url": "/MemoryGame/img/home.4e573337.svg"
+  },
+  {
+    "revision": "23d5b217b0a50efd347ca2b1508d1777",
+    "url": "/MemoryGame/img/party.23d5b217.svg"
+  },
+  {
+    "revision": "70d185fa2c4ad05121f35164574c0ef3",
+    "url": "/MemoryGame/img/play.70d185fa.svg"
+  },
+  {
+    "revision": "8d996083b0e1c6c20a5c598598d72b2c",
+    "url": "/MemoryGame/img/repeat.8d996083.svg"
+  },
+  {
+    "revision": "bd541fcb44c47e95ce3a5decba27a1c5",
+    "url": "/MemoryGame/index.html"
+  },
+  {
+    "revision": "aae8670761a015962be2",
+    "url": "/MemoryGame/js/app.ba490a41.js"
+  },
+  {
+    "revision": "79781023bb2f271a1944",
+    "url": "/MemoryGame/js/chunk-vendors.d11ef624.js"
+  },
+  {
+    "revision": "3d7a30651d60308dfdf1986f7f8ab3de",
+    "url": "/MemoryGame/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MemoryGame/robots.txt"
+  }
+]);
